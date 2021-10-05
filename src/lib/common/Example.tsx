f19e2f5348a15fcdd785dcf9e95046e63ee10da6
Agung Yuliyanto <agung.96tm@gmail.com>
2021-10-05 20:54:26 +0700

    initial first (2)

---
+++ src/lib/common/Example.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example: React.FC<unknown> = () => {
+    return (
+        <div>Example</div>
+    )
+}
+
+export default Example;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,2 @@
-// @ts-nocheck
 export * from './common';
-export * from './seo';
+
