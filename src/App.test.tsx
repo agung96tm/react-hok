db9963555e94209d0b6b0a1537d0154597f711ab
Agung Yuliyanto <agung.96tm@gmail.com>
2021-10-05 22:25:15 +0700

    setup travis, tsconfig, build and test

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
-
-
-test('Should be say "hello world"', () => {
-    expect("hello world").toBe("hello world");
-});
--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-export * from './common';
-export * from './seo';
